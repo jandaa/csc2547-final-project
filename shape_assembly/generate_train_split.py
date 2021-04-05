2a8a070561ca7ef49e79d3083c969b16fb0a4210
jandaa <andrej.janda@gmail.com>
2021-04-05 10:38:03 -0400

    Update shape assembly specs

--- shape_assembly/generate_train_split.py
+++ shape_assembly/generate_train_split.py
@@ -1,11 +1,7 @@
 import json
-import jsbeautifier
 from pathlib import Path
 
-opts = jsbeautifier.default_options()
-opts.indent_size = 2
-
-train_split = [str(i) for i in range(900)]
+train_split = ["val_" + str(i) for i in range(900)]
 
 train_split_file = Path("test_split.json")
 with train_split_file.open(mode="w") as f:
